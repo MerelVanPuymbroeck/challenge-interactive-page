84947eead09377fe0446abd3ce2c64954765e0fd
Merelvanpuymbroeck <Merelvanpuymbroeck@gmail.com>
2019-08-01 11:15:03 +0200

    Tabs are working!

---
+++ script.js
@@ -0,0 +1,26 @@
+function myFunctionone() {
+    var x = document.getElementById("summary");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunctiontwo() {
+    var x = document.getElementById("practical");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunctionthree() {
+    var x = document.getElementById("contact");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
